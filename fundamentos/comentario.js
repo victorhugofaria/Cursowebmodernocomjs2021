19793af3335f2b59c4fc7f912e3c5d71f0447afa
Victor Hugo Faria da Silva <64808653+victorhugofaria@users.noreply.github.com>
2021-05-26 10:05:52 -0400

    addfundamentos

---
+++ fundamentos/comentario.js
@@ -0,0 +1,12 @@
+// Comentario de uma linha
+console.log("Linha 1")1
+
+/*Comentário de múltiplas linhas 
+*/ 
+console.log("linha 2")
+
+/*
+*Comentário de múltiplas linhas
+*
+*/
+console.log("linha 3")
