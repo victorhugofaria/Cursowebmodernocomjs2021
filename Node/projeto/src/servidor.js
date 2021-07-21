b92fe7c5c34a703d3c506a0856087f6ea6916555
Victor Hugo Faria da Silva <victorcobain@hotmail.com>
2021-07-21 13:19:24 -0400

    addnewfunctions

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,11 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancodeDados = require('./bancoDeDados')
+
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: "Notebook", preco: 123.45}) // Converter para JSON
+    res.send(bancodeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+      res.send(bancodeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next)=>{
+    const produto = bancodeDados.salvarProduto({
+        nome:  req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) // JSON
 })
 
 app.listen(porta, () => {
-    console.log(`Servidor executando na porta ${porta}.`)
+    console.log(`Servidor  está executando na porta ${porta}.`)
 })
