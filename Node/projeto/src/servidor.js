47e5fef5ca4b1b17ee9ed720ed3f97e9d2e76da4
Victor Hugo Faria da Silva <victorcobain@hotmail.com>
2021-07-22 13:14:27 -0400

    addtemporizador

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,8 +2,10 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const bancodeDados = require('./bancoDeDados')
 
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/produtos', (req, res, next) => {
     res.send(bancodeDados.getProdutos())
@@ -13,14 +15,19 @@ app.get('/produtos/:id', (req, res, next) => {
       res.send(bancodeDados.getProduto(req.params.id))
 })
 
-app.post('/produtos', (req, res, next)=>{
+app.put('/produtos/:id', (req, res, next)=>{
     const produto = bancodeDados.salvarProduto({
-        nome:  req.body.name,
+        id: req.params.id,
+        nome:  req.body.nome,
         preco: req.body.preco
     })
     res.send(produto) // JSON
 })
 
+app.delete('./produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto) // JSON
+})
 app.listen(porta, () => {
     console.log(`Servidor  está executando na porta ${porta}.`)
 })
