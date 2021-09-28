6ee68c828fc140cbdb7ece34a31ee634b97e5540
Victor Hugo Faria da Silva <victorcobain@hotmail.com>
2021-09-28 13:50:58 -0400

    movearchiveswithgulp

---
+++ gulp/basico/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp')
+const {series, parallel} = require('gulp')
+
+
+const antes1 = cb => {
+    console.log('Tarefa Antes 1!')
+    return cb()
+}
+
+
+const antes2 = cb => {
+    console.log('Tarefa Antes 2!')
+    return cb()
+}
+
+const fim = cb => {
+    console.log('Tarefa Fim!')
+    return cb()
+}
+
+
+function copiar(cb) {
+    gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt']) // pegando arquivos //
+    .pipe(gulp.dest('pastaB')) //copiando para a pastaB //
+    return cb()
+}
+
+module.exports.default = series (
+    parallel(antes1, antes2),
+    copiar,
+    fim,
+)
